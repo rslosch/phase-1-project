61bcf0a9ca926cf1b4f1ed0ce31b7c899141cf3c
Steven <slosch99@gmail.com>
2022-04-13 16:12:33 -0400

    Cleaned up comments

--- javascripts/index.js
+++ javascripts/index.js
@@ -7,6 +7,7 @@ window.addEventListener('DOMContentLoaded', () => {
     displayCoins()
     })
 
+
 function fetchCoins(){
     fetch('https://api.coincap.io/v2/assets')
     .then(res => res.json())
@@ -16,7 +17,7 @@ function fetchCoins(){
 }
 
 function displayCoins(){ 
-    let btn = document.getElementById('view-tokens')
+    const btn = document.getElementById('view-tokens')
     btn.addEventListener("click", (e) => {
         const ul = document.getElementById('token-list')
         coins.forEach(coin => {
@@ -40,11 +41,8 @@ const displayIndividualCoin = (e) => {
     const info = document.getElementById("coin-info")
     const ul = document.getElementById('token-list')
     ul.innerHTML = ""
-    // fetch(`https://api.coincap.io/v2/assets/${e.target.dataset.id}`)
-    // .then(res => res.json())
-    // .then(data => {
     const coin = coins.find(c => c.id === e.target.dataset.id)
-    // const {name, symbol, rank, marketCapUsd, priceUsd, changePercent24Hr} = data.data
+    // const {name, symbol, rank, marketCapUsd, priceUsd, changePercent24Hr} = coin
     console.log(coin)
     info.innerHTML = `
         <h1>Coin: ${coin.name}</h1>
@@ -55,3 +53,51 @@ const displayIndividualCoin = (e) => {
         <h3>24hr Price Change: ${coin.changePercent24Hr.slice(0,4)}%</h3>
     `
 }
+
+const animals = [{
+    isPet: true,
+    type: "dog",
+    name: "Olive",
+  }, 
+  {
+    isPet: false,
+    type: "lion",
+    name: "Nala",
+  },
+  {
+    isPet: false,
+    type: "bear",
+    name: "Honey",
+  },
+  {
+    isPet: true,
+    type: "cat",
+    name: "Maru"
+  },{
+    isPet: true,
+    type: "fish",
+    name: "Dr. Fishy"
+  }]
+
+// sort through animals array and select the animals with isPet===true
+// append and create btn to document body querySelector(#body)
+// add onClick event listener which console logs name
+
+
+const petAnimals = animals.forEach(pet => {
+    const body = document.body
+
+    if(pet.isPet === true) {
+        console.log(pet)
+        const btn = document.createElement('button')
+        btn.innerHTML = pet.type
+        body.appendChild(btn)
+        btn.addEventListener('click', () => {
+            console.log(pet.name)
+        })
+    }
+})  
+  
+  
+  
+  
